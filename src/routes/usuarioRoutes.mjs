c9880b3b64366a60f929216886b97d30dc61cb36
Lautaro Exequiel Tapia <lautaro.online@gmail.com>
2025-08-27 20:30:04 -0300

    fix: importar handleValidationErrors en usuarioRoutes

--- src/routes/usuarioRoutes.mjs
+++ src/routes/usuarioRoutes.mjs
@@ -6,6 +6,7 @@ import {
     actualizarUsuarioController,
     eliminarUsuarioController
 } from '../controllers/usuarioController.mjs';
+import { handleValidationErrors } from '../middlewares/validations.mjs';
 
 const router = express.Router();
 
