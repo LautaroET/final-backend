ed2be887d7f4d4294fccef9035b5b8a3ce4e9013
Lautaro Exequiel Tapia <lautaro.online@gmail.com>
2025-08-27 01:57:57 -0300

    Add server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { connectDB } from './src/config/dbConfig.mjs';
+import usuarioRoutes from './src/routes/usuarioRoutes.mjs';
+import refugioRoutes from './src/routes/refugioRoutes.mjs';
+import mascotaRoutes from './src/routes/mascotaRoutes.mjs';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+connectDB();
+
+app.use('/api', usuarioRoutes);
+app.use('/api', refugioRoutes);
+app.use('/api', mascotaRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({ mensaje: 'Ruta no encontrada' });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🐾 Servidor de Patitas al Rescate escuchando en puerto ${PORT}`);
+});
